f154108580451bed4ec77734db4eecfcca1ae6e4
Hemant V. Torsekar <your_email@example.com>
2013-03-19 15:44:11 -0400

    First commit performed, first push.

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,13 @@
+class PicturesController < ApplicationController
+
+	def index	
+		@greeting = "Hello World!"
+
+		@pictures = [
+	    	'http://monicao.s3.amazonaws.com/bitmaker/house.jpg',
+	     	'http://monicao.s3.amazonaws.com/bitmaker/wave.jpg',
+	     	'http://monicao.s3.amazonaws.com/bitmaker/girl.jpg'
+	 	]
+	end
+end
+	
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photogur::Application.config.secret_token = 'a915910f74d7b46bd4dc24c5938798d7dc8e9b01c784c0c6895fa4567e053d332e7ddbdf8ad54a5a654991946eb71e7cf545a6f95c7d73d70082ada6bb86c381'
